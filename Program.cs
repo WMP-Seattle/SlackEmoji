a0c82ca3efbd5ac876eabd40a2afdc5191a36cb3
Cameron Bruce <cbruce@sea-cbruce.local>
2018-01-22 13:59:02 -0800

    adding program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace slackemoji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
